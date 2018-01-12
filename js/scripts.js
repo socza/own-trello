3be6c3dd6068e805ed57179c0248f6b917a3c381
Piotr Sociński <psocinski@gmail.com>
2018-01-12 12:30:36 +0100

    Added function randomString() whith is related for value id class Column(). Added Column() class.

---
+++ js/scripts.js
@@ -0,0 +1,37 @@
+$(function() {
+
+	function randomString() {
+
+		var chars  = '0123456789abcdefghiklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXTZ';
+		var str = '';
+		
+		for (i = 0; i < 10; i++) {
+
+			str += chars[Match.floor(Match.random() * chars.length)];
+
+		}
+
+		return str;
+
+	}
+
+	function Column(name) {
+
+		var self = this;
+		this.id = randomString();
+		this.name = name;
+		this.$element = createColumn();
+
+		function createColumn() {
+
+			var $column = $('<div>').addClass('column');
+			var $columnTitle = $('<h1>').addClass('column-title').text(self.name);
+			var $columnCardList = $('<ul').addClass('column-card-list');
+			var $columnCardDelete = $('<button>').addClass('btn-delete').text('x');
+			var $columnAddCard = $('<button>').addClass('add-card').text('Add a card');
+
+		}
+
+	}
+
+});
